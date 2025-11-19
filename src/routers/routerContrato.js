3b0b86ac161f08dc15085ffca36fa096c839af55
Matias <maticampopiano@hotmail.com>
2025-11-19 17:08:43 -0300

    arreglo la llamada y el metodo de obtener contratos No ocupados

--- src/routers/routerContrato.js
+++ src/routers/routerContrato.js
@@ -3,7 +3,7 @@ const routerContrato= express.Router();
 const contractController = require('../controllers/contractController');
 
 routerContrato.get('/contratos',contractController.readContracts)
-routerContrato.get('/contratoslibres',contractController.readContracts)
+routerContrato.get('/contratoslibres',contractController.readNonOccupiedContracts)
 routerContrato.get('/contratos/:emailgerente', contractController.readContractsByGerenteEmail)
 routerContrato.post('/contrato',contractController.createContract)
 routerContrato.put("/contrato/:id/", contractController.updateContract)
